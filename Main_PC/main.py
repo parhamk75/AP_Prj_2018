537ef03dfa9fff53f33f291d6ca915dc5fa1dd2f
parhamk75 <36754636+parhamk75@users.noreply.github.com>
2018-07-14 21:01:10 +0430

    Form Created!

--- Main_PC/main.py
+++ Main_PC/main.py
@@ -3,18 +3,29 @@ from queue import Queue
 from threading import Thread
 import re
 
+import os
+import sys
 
+from PyQt5 import uic
 
+import matplotlib
+matplotlib.use("Qt5Agg")
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
 
+################## Serial ####################
 
-Serial_Port = "COM7"
+Serial_Port = "COM1"
 Baud_Rate = 57600
 
 Input_Buf = Queue()
 
-Data_Buf_Raw = Queue()
+#Data_Buf_Raw = Queue()
 
 Data_Buf_Processed = Queue()
+
 
 Packet_ID_RE = re.compile('165,90,([0-9]{1,3}),([0-9]{1,3}),([0-9]{1,3}),([0-9]{1,3}),1,')
 #       Please Don't omit the last comma (',') in the RE above because it
@@ -58,7 +69,7 @@ class pckt_xtrctr_thrd(Thread):
             tmp_2 = Packet_ID_RE.findall(tmp_1)            
             
             if len(tmp_2) > 0:
-                Data_Buf_Raw.put(tmp_2[0])                
+#                Data_Buf_Raw.put(tmp_2[0])                
                 tmp_1 = ''        
 #                Unpacking the raw data to the processed data queue
                 num = int( (tmp_2[0])[0] )*256 + int( (tmp_2[0])[1] )
@@ -67,10 +78,40 @@ class pckt_xtrctr_thrd(Thread):
             i += 1
             
         print(tmp_1)
- 
+###########################################
+        
+################## GUI ####################
+        
+Forms = uic.loadUiType( os.path.join( os.getcwd(), 'Ploter.ui') )
     
+class Ploter( Forms[0], QMainWindow):
+    def __init__(self):
+        Forms[0].__init__(self)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        
+        
+        self.Tmp_Run_Bttn.clicked.connect(self.Tmp_Run_Bttn_Func)
+        
+        self.fig = Figure()
+        self.ax = self.fig.add_axes([ 0.1, 0.1, 0.8, 0.8])
+        self.canvas = FigureCanvas(self.fig)
+        self.navi = NavigationToolbar( self.canvas, self)
+        
+        l = QVBoxLayout(self.Plt_Wdgt)
+        l.addWidget(self.canvas)
+        l.addWidget(self.navi)
+        
+    def Tmp_Run_Bttn_Func(self):
+        print("Ich liebe sie!")
+        
+###########################################
 
-EMG_Shield_Serial_Reader = sril_thrd("COM7", 57600)
+
+
+############## Main Thread ################
+
+EMG_Shield_Serial_Reader = sril_thrd( Serial_Port, Baud_Rate)
 EMG_Shield_Serial_Reader.start()
 
 EMG_Data_Packet_Extractor = pckt_xtrctr_thrd()
@@ -81,9 +122,15 @@ EMG_Shield_Serial_Reader.join()
 EMG_Data_Packet_Extractor.join()
 
 
-for i in range(Data_Buf_Raw.qsize()):
+for i in range(Data_Buf_Processed.qsize()):
     print(Data_Buf_Processed.get())
         
+
+pltr_app = QApplication(sys.argv)
+pltr_w = Ploter()
+pltr_w.show()
+
+sys.exit( pltr_app.exec_() )
         
-    
+###########################################    
     
